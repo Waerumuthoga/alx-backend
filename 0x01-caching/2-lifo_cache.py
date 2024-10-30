a07135d37949690787bbc59439c3c884191757c8
Waerumuthoga <waerumuthoga@gmail.com>
2024-10-30 20:22:02 +0000

    task 2

---
+++ 0x01-caching/2-lifo_cache.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+""" 2-lifo_cache """
+from base_caching import BaseCaching
+
+
+class LIFOCache(BaseCaching):
+    """LifoCache dictionary"""
+
+    def __init__(self):
+        """Constructor"""
+        super().__init__()
+        self.queue = []
+
+    def put(self, key, item):
+        """Add an item in the cache"""
+        if key is not None and item is not None:
+            if key in self.cache_data:
+                self.queue.remove(key)
+            elif len(self.cache_data) >= self.MAX_ITEMS:
+                discard = self.queue.pop()
+                del self.cache_data[discard]
+                print("DISCARD: {}".format(discard))
+            self.queue.append(key)
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """Get an item by key"""
+        if key is None or key not in self.cache_data.keys():
+            return None
+        return self.cache_data[key]
